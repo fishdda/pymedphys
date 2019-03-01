5035e8ceacf9861e865adcad9a1ecaad9469837f
Simon Biggs <me@simonbiggs.net>
2019-03-01 22:20:08 +1100

    create cupy fallback class

---
+++ src/pymedphys/cupy/__init__.py
@@ -0,0 +1,16 @@
+import numpy
+
+try:
+    import cupy
+
+    class CupyNumpyFallback:
+        def __getattr__(self, key):
+            try:
+                return getattr(cupy, key)
+            except AttributeError:
+                return getattr(numpy, key)
+
+    cp = CupyNumpyFallback()
+
+except ImportError:
+    cp = numpy
